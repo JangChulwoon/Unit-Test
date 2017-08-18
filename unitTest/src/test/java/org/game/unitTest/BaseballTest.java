b17b7eb3edb3b193c5215302d83ee00fc038c9ea
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-08-18 17:00:53 +0900

    step 01 돌아가지 않는 코드

--- unitTest/src/test/java/org/game/unitTest/BaseballTest.java
+++ unitTest/src/test/java/org/game/unitTest/BaseballTest.java
@@ -1,5 +1,20 @@
 package org.game.unitTest;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class BaseballTest {
 
+	@Test
+	public void shouldNullCheck() {
+		Game game = new Game();
+		
+		try {
+			game.vaild(null);
+			Assert.fail();
+		}catch (IllegalArgumentException e) {
+			// TODO: handle exception
+		}
+	}
 }
