7d8e762589c2221dfcd8461b2e265e1c404ca85a
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-08-18 14:29:37 +0900

    [step 01 ] 설계 - Text로 설계한다.

--- unitTest/src/test/java/org/baseball/unitTest/BaseballTest.java
+++ unitTest/src/test/java/org/baseball/unitTest/BaseballTest.java
@@ -4,8 +4,23 @@ import org.junit.Test;
 
 public class BaseballTest {
 	
+	/**
+	 * Baseball Game 
+	 * 1. 임의의 숫자 4개를 생성한다.
+	 * 2. 사용자는 임의  4개를 입력한다.
+	 * 2-1 만약 공통된 부분이 있다면  strike ( 개수 )
+	 * 2-2 공통됬지만 위치가 다르다면 ball 
+	 * 2-3 공통된 수가 없으면 out 
+	 * 3. 기회는 9번
+	 * 4. 모두다 맞으면 게임은 끝난다.  
+	 */
 	@Test
 	public void shouldBaseballTest() {
+		// 필요한 함수.
+		// 1. 숫자를 생성하는 함수.
+		// 2. 수를 입력받는 함수.
+		// 3. 확인하는 함수. 
+		
 		
 	}
 
