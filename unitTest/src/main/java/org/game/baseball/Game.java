f712ba3a326692cff4dc36acc622f72e97e13e9f
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-08-18 17:05:12 +0900

    step 3 초록 코드 만들기

--- unitTest/src/main/java/org/game/baseball/Game.java
+++ unitTest/src/main/java/org/game/baseball/Game.java
@@ -1,7 +1,12 @@
 package org.game.baseball;
 
+import org.junit.internal.Throwables;
+
 public class Game {
 	public void vaild(String value) {
+		if(value == null) {
+			throw new IllegalArgumentException();
+		}
 		
 	}
 }
