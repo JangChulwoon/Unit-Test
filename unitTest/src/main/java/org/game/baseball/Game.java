4eebedaaa086dffbd9ce0287d7c697c00f795ccc
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-08-18 17:13:37 +0900

    step 3 초록색 코드 만들기.

--- unitTest/src/main/java/org/game/baseball/Game.java
+++ unitTest/src/main/java/org/game/baseball/Game.java
@@ -4,9 +4,13 @@ import org.junit.internal.Throwables;
 
 public class Game {
 	public void vaild(String value) {
-		if(value == null) {
+		if (value == null) {
 			throw new IllegalArgumentException();
 		}
-		
+		for (char input : value.toCharArray()) {
+			if (input < '0' || input > '9') {
+				throw new IllegalArgumentException();
+			}
+		}
 	}
 }
